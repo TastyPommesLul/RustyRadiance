ee8c5df60bb665366d21dc254d093c320489b3bf
TastyPommesLul <ernstd.privat@gmail.com>
2026-05-03 10:04:00 +0200

    changed some stuff

--- src/main/java/dev/tastypommeslul/rustyradiance/RustyRadiance.java
+++ src/main/java/dev/tastypommeslul/rustyradiance/RustyRadiance.java
@@ -1,32 +1,16 @@
 package dev.tastypommeslul.rustyradiance;
 
-import net.minecraft.world.item.*;
-import org.slf4j.Logger;
-
 import com.mojang.logging.LogUtils;
-
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
-import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
-import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import net.neoforged.neoforge.registries.DeferredBlock;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredItem;
-import net.neoforged.neoforge.registries.DeferredRegister;
+import org.slf4j.Logger;
 
 @Mod(RustyRadiance.MODID)
 public class RustyRadiance {
@@ -34,6 +18,7 @@ public class RustyRadiance {
     public static final Logger LOGGER = LogUtils.getLogger();
     public RustyRadiance(IEventBus modEventBus, ModContainer modContainer) {
         modEventBus.addListener(this::commonSetup);
+        modEventBus.addListener(this::addCreative);
 
         NeoForge.EVENT_BUS.register(this);
 
@@ -44,7 +29,6 @@ public class RustyRadiance {
 
     }
 
-    @SubscribeEvent
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
 
     }
